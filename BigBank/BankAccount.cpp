64d2e6882dbf24c24c16715b6772209598cb621a
Butescu Ilaria Teodora <162502914+ButescuIlariaTeodora@users.noreply.github.com>
2024-04-16 20:16:53 +0300

    Add files via upload

---
+++ BigBank/BankAccount.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "pch.h"
+#include "BankAccount.h"
+
+BankAccount::BankAccount(String^ holder)
+	: accountHolder(holder), balance(0.0)
+{
+
+}
+void BankAccount::Credit(double amount)
+{
+	balance += amount;
+}
+bool BankAccount::Debit(double amount)
+{
+	if (CanDebit(amount))
+	{
+		balance -= amount;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+double BankAccount::GetBalance()
+{
+	return balance;
+}
+String^ BankAccount::RoutingInstructions(double amount)
+{ 
+	return "Some string..";
+}
